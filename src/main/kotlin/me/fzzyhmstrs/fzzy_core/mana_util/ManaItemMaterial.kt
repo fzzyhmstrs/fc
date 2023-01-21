0d1e4b99d8a4d5836bbcf0d8bf139a36dde9552e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-01-21 17:18:01 -0500

    Complete refactor and rebuild of the fzzy core structure, taking out all the gear and magic stuff

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/mana_util/ManaItemMaterial.kt
@@ -0,0 +1,15 @@
+package me.fzzyhmstrs.fzzy_core.mana_util
+
+/**
+ * tool material to pair with a [ManaItem]
+ */
+interface ManaItemMaterial {
+
+    fun minCooldown(): Long{
+        return 20L
+    }
+    fun baseCooldown(): Long{
+        return 150L
+    }
+    fun healCooldown(): Long
+}
