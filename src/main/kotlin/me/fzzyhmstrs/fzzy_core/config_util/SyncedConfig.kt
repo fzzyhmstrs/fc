a66513d16c1f7d20f3aa42bfbe66dafd95362231
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-23 08:54:21 -0500

    Update SyncedConfig.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/SyncedConfig.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/SyncedConfig.kt
@@ -2,8 +2,6 @@ package me.fzzyhmstrs.fzzy_core.config_util
 
 import net.minecraft.network.PacketByteBuf
 
-interface SyncedConfig{
-    fun readFromServer(buf: PacketByteBuf)
-    fun writeToClient(buf: PacketByteBuf)
-    fun initConfig()
+abstract class SyncedConfig: ClientServerSynced{
+    abstract fun initConfig()
 }
