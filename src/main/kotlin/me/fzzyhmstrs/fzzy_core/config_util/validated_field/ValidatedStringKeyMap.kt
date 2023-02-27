0b7a50a5d0efb573ba19f05be97ec3f242e88b08
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-27 10:24:14 -0500

    Update ValidatedStringKeyMap.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/validated_field/ValidatedStringKeyMap.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/validated_field/ValidatedStringKeyMap.kt
@@ -3,7 +3,7 @@ package me.fzzyhmstrs.fzzy_core.config_util.validated_field
 import me.fzzyhmstrs.fzzy_core.config_util.SyncedConfigHelperV1
 import java.util.function.BiPredicate
 
-class ValidatedStringKeyMap<T>(
+open class ValidatedStringKeyMap<T>(
     defaultValue:Map<String,T>,
     type:Class<T>,
     mapEntryValidator: BiPredicate<String, T> = BiPredicate{ _, _ -> true},
