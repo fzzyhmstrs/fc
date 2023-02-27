452d433968e815c6f8cfea38cbf478c0a067501a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-02-27 13:11:25 -0500

    Update ValidatedStringList.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/validated_field/ValidatedStringList.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/validated_field/ValidatedStringList.kt
@@ -1,7 +1,7 @@
 package me.fzzyhmstrs.fzzy_core.config_util.validated_field
 
 import me.fzzyhmstrs.fzzy_core.config_util.SyncedConfigHelperV1
-import java.util.function.BiPredicate
+import java.util.function.Predicate
 
 open class ValidatedStringList(
     defaultValue:List<String>,
