02d237e62a9e5fd1609af4361e648d6ab4fa064a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-02 13:57:11 -0500

    Create Animatable.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/config_util/gui/Animatable.kt
@@ -0,0 +1,8 @@
+package me.fzzyhmstrs.fzzy_core.config_util.gui
+
+abstract class Animatable{
+    fun hoveredFrame(): Frame
+    fun unHoveredFrame(): Frame
+    fun clickedFrame(): Frame
+    fun releasedFrame(): Frame
+}
