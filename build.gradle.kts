a419a4e9d7283ca21b9bb69df0d3287c6cfc98c1
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-03-03 13:52:48 -0500

    remove changelog.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,15 @@ plugins {
     id("fabric-loom")
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
-    id("com.modrinth.minotaur") version "2.+"
 }
 base {
     val archivesBaseName: String by project
     archivesName.set(archivesBaseName)
 }
-val log: File = file("changelog.md")
 val modVersion: String by project
 version = modVersion
 val mavenGroup: String by project
 group = mavenGroup
-println("## Changelog for Amethyst Core $modVersion \n\n" + log.readText())
 repositories {
     maven {
         name = "TerraformersMC"
@@ -78,23 +75,4 @@ tasks {
         targetCompatibility = javaVersion
         withSourcesJar()
     }
-}
-
-modrinth {
-    token.set(System.getenv("MODRINTH_TOKEN"))
-    projectId.set("fzzy-core")
-    versionNumber.set(modVersion)
-    versionName.set("${base.archivesName.get()}-$modVersion")
-    versionType.set("beta")
-    uploadFile.set(tasks.remapJar.get())
-    gameVersions.addAll("1.19.3")
-    loaders.addAll("fabric","quilt")
-    detectLoaders.set(false)
-    changelog.set("## Changelog for Fzzy Core $modVersion \n\n" + log.readText())
-    dependencies{
-        required.project("fabric-api")
-        required.project("fabric-language-kotlin")
-        optional.project("trinkets")
-    }
-    debugMode.set(true)
 }
