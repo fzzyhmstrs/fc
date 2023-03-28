c2d1befa5ebd1f48d43d9c5974ecdbf43018f6ae
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-03-27 22:26:21 -0400

    tweak CustomFlavorToolItem.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/item_util/CustomFlavorToolItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/item_util/CustomFlavorToolItem.kt
@@ -30,15 +30,21 @@ open class CustomFlavorToolItem(material: ToolMaterial,settings: Settings) : Too
     private val flavorTextDesc: MutableText by lazy{
         makeFlavorTextDesc()
     }
-    
+
     private fun makeFlavorText(): MutableText{
         val id = Registries.ITEM.getId(this)
-        return AcText.translatable("item.${id.namespace}.${id.path}.flavor").formatted(Formatting.WHITE, Formatting.ITALIC)
+        val key = "item.${id.namespace}.${id.path}.flavor"
+        val text = AcText.translatable(key).formatted(Formatting.WHITE, Formatting.ITALIC)
+        if (text.string == key) return AcText.empty()
+        return text
     }
-    
+
     private fun makeFlavorTextDesc(): MutableText{
         val id = Registries.ITEM.getId(this)
-        return AcText.translatable("item.${id.namespace}.${id.path}.flavor.desc").formatted(Formatting.WHITE)
+        val key = "item.${id.namespace}.${id.path}.flavor.desc"
+        val text = AcText.translatable(key).formatted(Formatting.WHITE)
+        if (text.string == key) return AcText.empty()
+        return text
     }
 
     override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
