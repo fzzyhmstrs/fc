57deb4708bef08c9c34bf72d2f8d3c32b1a48140
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-06-01 11:56:41 -0400

    updated PersistentEffectHelper.kt with a server-triggered clear out, and dustbin with Vectors to help with thread safety

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/coding_util/PersistentEffectHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/coding_util/PersistentEffectHelper.kt
@@ -1,22 +1,22 @@
 package me.fzzyhmstrs.fzzy_core.coding_util
 
 import me.fzzyhmstrs.fzzy_core.registry.EventRegistry
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Utilities for implementing a persistent effect that isn't linked to a particular object. Once initiated, the object that called it does not necessarily have to continue existing or performing the action that initiated the persistent effect. This is useful in instances where you want an effect to occur over time, but don't want to deal with the headache of coding and managing tracking that persistent effect. All of that is handled here.
  */
 object PersistentEffectHelper {
 
-    private val persistentEffects: MutableList<PersistentEffectInstance> = mutableListOf()
-    private val persistentEffectQueue: Vector<PersistentEffectInstance> = Vector()
+    private val persistentEffects: Vector<PersistentEffectInstance> = Vector()
     private val DUSTBIN = Dustbin { instance: PersistentEffectInstance ->
-        persistentEffects.remove(instance); if (persistentEffects.isEmpty()) {
-        persistentEffectsFlag = false
+            persistentEffects.remove(instance); if (persistentEffects.isEmpty()) {
+            persistentEffectsFlag.set(false)
+        }
     }
-    }
-    private var persistentEffectsFlag: Boolean = false
-    private var locked = false
+    private var persistentEffectsFlag: AtomicBoolean = AtomicBoolean(false)
 
     /**
      * primary function of interest. Basically registers a persistent effect with the built-in ticker for execution. You can pass custom implementations of [PersistentEffectData] and [PersistentEffect] to the parameters as needed.
@@ -28,27 +28,26 @@ object PersistentEffectHelper {
         delay: Int, duration: Int,
         data: PersistentEffectData
     ){
-        if (!locked) {
-            persistentEffects.add(PersistentEffectInstance(EventRegistry.Ticker(delay), delay, duration, augment, data))
-            persistentEffectsFlag = true
-        } else {
-            persistentEffectQueue.add(PersistentEffectInstance(EventRegistry.Ticker(delay), delay, duration, augment, data))
-        }
+        persistentEffects.add(PersistentEffectInstance(EventRegistry.Ticker(delay), delay, duration, augment, data))
+        persistentEffectsFlag.set(true)
+    }
 
+    fun registerServer(){
+        ServerLifecycleEvents.SERVER_STOPPING.register {
+            clearPersistentEffects()
+        }
+    }
+
+    private fun clearPersistentEffects(){
+        while(persistentEffectsFlag.get()){
+            persistentEffectTicker()
+        }
     }
 
     internal fun persistentEffectTicker(){
-        locked = true
         DUSTBIN.clean()
-        if (!persistentEffectsFlag) {
-            locked = false
-            if (persistentEffectQueue.isNotEmpty()) {
-                persistentEffects.addAll(persistentEffectQueue)
-                persistentEffectsFlag = true
-                persistentEffectQueue.clear()
-            } else {
-                return
-            }
+        if (!persistentEffectsFlag.get()) {
+            return
         }
         for (i in 0 until persistentEffects.size) {
             val it = persistentEffects[i]
@@ -64,13 +63,6 @@ object PersistentEffectHelper {
                 }
             }
         }
-        locked = false
-        if (persistentEffectQueue.isNotEmpty()) {
-            persistentEffects.addAll(persistentEffectQueue)
-            persistentEffectsFlag = true
-            persistentEffectQueue.clear()
-        }
-
     }
 
     private data class PersistentEffectInstance(val ticker: EventRegistry.Ticker, val delay: Int, var duration: Int, val augment: PersistentEffect, val data: PersistentEffectData)
