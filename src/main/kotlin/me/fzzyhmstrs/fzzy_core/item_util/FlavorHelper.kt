9841c6315da471b8f7d2720a5ca017573fd5ec03
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-08-20 11:27:10 -0400

    fixes

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/item_util/FlavorHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/item_util/FlavorHelper.kt
@@ -4,15 +4,16 @@ import me.fzzyhmstrs.fzzy_core.coding_util.AcText
 import me.fzzyhmstrs.fzzy_core.config.FcConfig
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
-import net.minecraft.registry.Registries
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
+import net.minecraft.util.registry.Registry
+
 
 object FlavorHelper {
 
     fun makeFlavorText(item: Item): MutableText {
-        val id = Registries.ITEM.getId(item)
+        val id = Registry.ITEM.getId(item)
         val key = "item.${id.namespace}.${id.path}.flavor"
         val text = AcText.translatable(key).formatted(Formatting.WHITE, Formatting.ITALIC)
         if (text.string == key) return AcText.empty()
@@ -20,7 +21,7 @@ object FlavorHelper {
     }
 
     fun makeFlavorTextDesc(item: Item): MutableText {
-        val id = Registries.ITEM.getId(item)
+        val id = Registry.ITEM.getId(item)
         val key = "item.${id.namespace}.${id.path}.flavor.desc"
         val text = AcText.translatable(key).formatted(Formatting.WHITE)
         if (text.string == key) return AcText.empty()
