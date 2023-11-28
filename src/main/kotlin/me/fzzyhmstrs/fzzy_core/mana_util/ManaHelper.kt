101c5a7265427eacc336d10ad4b8d905ab326d36
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-11-27 22:07:20 -0500

    Add helper methods for getting and randomly healing ManaItem stacks to ManaHelper

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/mana_util/ManaHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/mana_util/ManaHelper.kt
@@ -2,7 +2,12 @@ package me.fzzyhmstrs.fzzy_core.mana_util
 
 import me.fzzyhmstrs.fzzy_core.nbt_util.Nbt
 import me.fzzyhmstrs.fzzy_core.registry.EventRegistry
+import me.fzzyhmstrs.fzzy_core.trinket_util.TrinketChecker
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
+import net.minecraft.world.World
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Helper object for mana items that self-heal. A simple initialization method that maps the provided stack to a [Ticker][me.fzzyhmstrs.fzzy_core.registry.EventRegistry.Ticker]
@@ -37,6 +42,46 @@ object ManaHelper {
     fun needsInitialization(stack: ItemStack): Boolean{
         val id = Nbt.getItemStackId(stack)
         return !healTickers.containsKey(id)
+    }
+
+    fun getManaItems(user: PlayerEntity): MutableList<ItemStack>{
+        val stacks: MutableList<ItemStack> = mutableListOf()
+        for (stack2 in user.inventory.main){
+            if (stack2.item is ManaItem && stack2.isDamaged){
+                stacks.add(stack2)
+            }
+        } // iterate over the inventory and look for items that are interfaced with "ManaItem"
+        for (stack2 in user.inventory.offHand){
+            if (stack2.item is ManaItem && stack2.isDamaged){
+                stacks.add(stack2)
+            }
+        }
+        for (stack2 in user.inventory.armor){
+            if (stack2.item is ManaItem && stack2.isDamaged){
+                stacks.add(stack2)
+            }
+        }
+        val stacks2 = TrinketChecker.getTrinketStacks(user)
+        stacks2.forEach {
+            if (it.item is ManaItem && it.isDamaged){
+                stacks.add(it)
+            }
+        }
+        return stacks
+    }
+
+    fun manaHealItems(list: MutableList<ItemStack>, world: World, healLeft: Int): Int{
+        var hl = healLeft
+        if (hl <= 0 || list.isEmpty()) return max(0,hl)
+        val rnd = world.random.nextInt(list.size)
+        val stack = list[rnd]
+        val healAmount = min(5,hl)
+        val healedAmount = (stack.item as? ManaItem)?.healDamage(healAmount,stack) ?: 0
+        hl -= min(healAmount,healedAmount)
+        if (!stack.isDamaged){
+            list.remove(stack)
+        }
+        return manaHealItems(list,world,hl)
     }
 
 }
