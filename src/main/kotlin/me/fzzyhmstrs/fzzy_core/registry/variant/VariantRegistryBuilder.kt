82ff9e44d8ae04a075695856949767729463611c
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-02-05 13:13:41 -0500

    tweak VariantRegistryBuilder

--- src/main/kotlin/me/fzzyhmstrs/fzzy_core/registry/variant/VariantRegistryBuilder.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_core/registry/variant/VariantRegistryBuilder.kt
@@ -10,28 +10,29 @@ import java.util.function.Function
 
 class VariantRegistryBuilder<T: Variant>(private val registryId: Identifier, private val defaultId: Identifier) {
 
-    private var tracked: Boolean = false
-
-    private var factory: Function<Identifier,T>? = null
-
-    fun simple(variantFactory: Function<Identifier,T>): VariantRegistryBuilder<T>{
-        this.factory = variantFactory
-        return this
-    }
-
-    fun tracked(): VariantRegistryBuilder<T>{
-        this.tracked = true
-        return this
-    }
-
     fun build(): VariantRegistry<T>{
         val registry: SimpleDefaultedRegistry<T> =  FabricRegistryBuilder.createDefaulted(
             RegistryKey.ofRegistry<T>(registryId), defaultId).buildAndRegister()
-        if (tracked){
-            val trackedData = TrackedDataHandler.of(registry).also { TrackedDataHandlerRegistry.register(it) }
-            return factory?.let { SimpleTrackedVariantRegistry(registry, it, trackedData) } ?: TrackedVariantRegistry<T>(registry, trackedData)
-        }
-        return factory?.let { SimpleVariantRegistry<T>(registry, it) } ?: VariantRegistry(registry)
+        return VariantRegistry(registry)
     }
 
+    fun buildSimple(variantFactory: Function<Identifier,T>): SimpleVariantRegistry<T>{
+        val registry: SimpleDefaultedRegistry<T> =  FabricRegistryBuilder.createDefaulted(
+            RegistryKey.ofRegistry<T>(registryId), defaultId).buildAndRegister()
+        return SimpleVariantRegistry<T>(registry, variantFactory)
+    }
+
+    fun buildTracked(): TrackedVariantRegistry<T>{
+        val registry: SimpleDefaultedRegistry<T> =  FabricRegistryBuilder.createDefaulted(
+            RegistryKey.ofRegistry<T>(registryId), defaultId).buildAndRegister()
+        val trackedData = TrackedDataHandler.of(registry).also { TrackedDataHandlerRegistry.register(it) }
+        return TrackedVariantRegistry<T>(registry, trackedData)
+    }
+
+    fun buildSimpleTracked(variantFactory: Function<Identifier,T>): SimpleTrackedVariantRegistry<T>{
+        val registry: SimpleDefaultedRegistry<T> =  FabricRegistryBuilder.createDefaulted(
+            RegistryKey.ofRegistry<T>(registryId), defaultId).buildAndRegister()
+        val trackedData = TrackedDataHandler.of(registry).also { TrackedDataHandlerRegistry.register(it) }
+        return SimpleTrackedVariantRegistry<T>(registry, variantFactory, trackedData)
+    }
 }
